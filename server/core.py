9b3444135f3c5fedd1869e699617c92b43913f27
Christopher Larsen <christopher.larsen@uconn.edu>
2014-10-03 23:13:22 -0400

    Added HTTP parsing

--- server/core.py
+++ server/core.py
@@ -1,14 +1,89 @@
 import argparse
 import threading
 import socketserver
-from .dumbserver import hi
 
 class HTTPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
+    allow_reuse_address = True
 
 class HTTPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        pass
+        try:
+            request = handleHTTP(self.request)
+            print(request)
+        except IOError as ex:
+            print("Malformed Request: " + str(ex))
+
+# Optimized for short lines
+def recv_until(sock, until, overflow='', scratchSpace=bytearray(1024)):
+    line = overflow
+    end = line.find(until)
+
+    while end == -1:
+        byteCount = sock.recv_into(scratchSpace)
+        if byteCount == 0:
+            raise IOError("Socket closed")
+        line += scratchSpace[0:byteCount].decode()
+        end = line.find(until)
+    end += len(until)
+
+    return line[0:end], line[end:]
+
+def handleHTTP(sock):
+    # Initialize read buffer
+    buff = bytearray(1024)
+
+    # Initialize parse structure
+    data = {}
+    data['headers'] = {}
+    data['raw'] = [] # Join at the end to prevent wasting time on new allocations
+
+    # Receive and parse the initial line
+    extra = ''
+    line, extra = recv_until(sock, '\r\n', extra, buff)
+    data['raw'].append(line)
+
+    # Break down the line into parts
+    parts = line.split(' ', 2)
+    if len(parts) != 3:
+        raise IOError("Malformed HTTP data")
+    parts[2] = parts[2][:-2] # Remove the trailing \r\n
+
+    # Determine if this is a request or a response
+    if len(parts[0]) > 4 and parts[0][0:4] == 'HTTP':
+        data['type'] = 'response'
+        data['version'] = parts[0]
+        try:
+            data['code'] = int(parts[1])
+        except ValueError:
+            raise IOError("Malformed HTTP response code")
+        data['codestr'] = parts[2]
+    else:
+        data['type'] = 'request'
+        data['verb'] = parts[0]
+        data['resource'] = parts[1]
+        data['version'] = parts[2]
+
+    while line != '\r\n':
+        line, extra = recv_until(sock, '\r\n', extra, buff)
+        data['raw'].append(line)
+        
+        # Try to parse the header if possible
+        parts = line.split(': ', 1)
+        if len(parts) == 2:
+            data['headers'][parts[0]] = parts[1][:-2]    
+
+    # Check if a body should be present
+    if 'Content-Length' in data['headers']:
+        clength = int(data['headers']['Content-Length']) - len(extra)
+        data['body'] = extra
+        if clength > 0:
+            data['body'] += sock.recv(clength)
+        
+        data['raw'].append(data['body'])
+
+    # Merge the response fields
+    data['raw'] = ''.join(data['raw'])
+    return data
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='A basic server')
@@ -16,22 +91,14 @@ def parseArgs():
     parser.add_argument('port', type=int, help='The port to bind the server to')
     return parser.parse_args()
 
-def startServer(host, port, root):
+def runServer(host, port, root):
     server = HTTPServer((host, port), HTTPRequestHandler)
-    serverThread = threading.Thread(target=server.serve_forever)
-    serverThread.start()
-    return server, serverThread
+    server.serve_forever()
 
 if __name__ == "__main__":
     params = parseArgs()
-    server, serverThread = startServer('localhost', params.port, params.root)
-    print("Server started")
+    print("Running server")
     try:
-        serverThread.join()
+        runServer('0.0.0.0', params.port, params.root)
     except KeyboardInterrupt:
-        print("Warm shutdown...")
-        server.shutdown()
-        try:
-            serverThread.join()
-        except KeyboardInterrupt:
-            print("Cold shutdown...")
+        print("Server shutdown")
